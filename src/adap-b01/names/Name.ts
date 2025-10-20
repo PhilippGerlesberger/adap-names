f77d15916c49b09197a902a1ef40f85d485e3d6f
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2025-10-20 15:44:13 +0200

    feat: Implement Name class.

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -7,37 +7,42 @@ export class Name {
     private delimiter: string = this.DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        this.delimiter = delimiter ? delimiter : this.DEFAULT_DELIMITER;
+        this.components = other.slice();
     }
 
     /** Returns human-readable representation of Name instance */
     public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let ret : string = this.components[0];
+        for (let i = 1; i < this.components.length; i++) {
+            ret += delimiter + this.components[i];
+        }
+        return ret;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
      public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components.splice(i, 0, c);
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        this.components.splice(i, 1);
     }
 
 }
