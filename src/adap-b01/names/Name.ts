051fd4a70de543a05886a9f0556f5e694a9881e5
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2025-10-22 15:05:01 +0200

    docs: Add @methodtype annotations to class methods

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,12 +6,14 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
+    // @methodtype: Initialization method
     constructor(other: string[], delimiter?: string) {
         this.delimiter = delimiter ? delimiter : this.DEFAULT_DELIMITER;
         this.components = other.slice();
     }
 
     /** Returns human-readable representation of Name instance */
+    // @methodtype: Conversion method
     public asNameString(delimiter: string = this.delimiter): string {
         let ret : string = this.components[0];
         for (let i = 1; i < this.components.length; i++) {
@@ -20,29 +22,34 @@ export class Name {
         return ret;
     }
 
+    // @methodtype: Get method
     public getComponent(i: number): string {
         return this.components[i];
     }
 
+    // @methodtype: Set method
     public setComponent(i: number, c: string): void {
         this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+     // @methodtype: Get method
      public getNoComponents(): number {
         return this.components.length;
     }
 
+    // @methodtype: Command method
     public insert(i: number, c: string): void {
         this.components.splice(i, 0, c);
     }
 
+    // @methodtype: Command method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype: Command method
     public remove(i: number): void {
         this.components.splice(i, 1);
     }
-
 }
