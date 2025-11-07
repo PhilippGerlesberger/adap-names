cd98c6c5c6eaa7ec25b729b23b359045ae000263
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2025-11-07 13:09:11 +0100

    feat: reuse/copy adjusted Constructer from adap_b01 Name class.

--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -1,20 +1,47 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 
+
+/**
+ * String Name class represents a name as a single string
+ */
 export class StringName implements Name {
 
     protected delimiter: string = DEFAULT_DELIMITER;
     protected name: string = "";
     protected noComponents: number = 0;
+    private special_characters: Set<string> = new Set([ESCAPE_CHARACTER, DEFAULT_DELIMITER]);
 
     constructor(source: string, delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        if (delimiter) {
+            if (delimiter.length !== 1) {
+                throw new Error("Delimiter must be a single character.")
+            }
+            this.delimiter = delimiter;
+            this.special_characters.add(delimiter);
+        }
+
+        this.name = source.slice();
     }
 
+    // --------------------------------------------------------------------------------------------
+    // String Representations
+    // --------------------------------------------------------------------------------------------
+
+    /**
+     * Returns a human-readable representation of the Name instance using user-set special characters
+     * Special characters are not escaped (creating a human-readable string)
+     * Users can vary the delimiter character to be used
+     */
     public asString(delimiter: string = this.delimiter): string {
         throw new Error("needs implementation or deletion");
     }
 
+    /**
+     * Returns a machine-readable representation of Name instance using default special characters
+     * Machine-readable means that from a data string, a Name can be parsed back in
+     * The special characters in the data string are the default characters
+     */
     public asDataString(): string {
         throw new Error("needs implementation or deletion");
     }
