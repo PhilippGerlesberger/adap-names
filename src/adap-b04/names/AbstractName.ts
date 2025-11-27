5aa0b9587bbf2fa1ffe8cbce8fe2f543cb1bfc51
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2025-11-27 15:13:12 +0100

    chore: add TODOs and preconditions checks for valid delimimter.

--- src/adap-b04/names/AbstractName.ts
+++ src/adap-b04/names/AbstractName.ts
@@ -1,3 +1,6 @@
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { NameParser } from "../parser/NameParser";
 import { Parser } from "../parser/Parser";
@@ -9,16 +12,23 @@ export abstract class AbstractName implements Name {
     protected nameParser: Parser;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        if (delimiter.length !== 1) {
-            throw new Error("Delimiter must be a single character.");
-        }
+        IllegalArgumentException.assert(this.isValidDelimiter(delimiter));
         this.delimiter = delimiter;
         this.nameParser = new NameParser(delimiter);
+
+        // TODO: Class invariant: nameParser
+        const newDelimiter = this.getDelimiterCharacter();
+        InvalidStateException.assert(this.isValidDelimiter(newDelimiter));
+
+        MethodFailedException.assert(newDelimiter == delimiter);
     }
 
     public clone(): Name {
         const clone = Object.create(Object.getPrototypeOf(this));
+        // TODO: Postcondition: Name object created?
+        // TODO: Class invarianz: check clone (isEqual, hashcode?)
         return Object.assign(clone, this);
+        
     }
 
     // --------------------------------------------------------------------------------------------
@@ -26,15 +36,18 @@ export abstract class AbstractName implements Name {
     // --------------------------------------------------------------------------------------------
 
     public asString(delimiter: string = this.delimiter): string {
+        IllegalArgumentException.assert(this.isValidDelimiter(delimiter));
         let unmaskComponents: string[] = [];
         for (let i = 0; i < this.getNoComponents(); i++) {
             const unmaskComponent = this.nameParser.unmask(this.getComponent(i));
             unmaskComponents.push(unmaskComponent);
         }
+        // TODO: Class invariant: valid unmask string?
         return unmaskComponents.join(delimiter);
     }
 
     public toString(): string {
+        // TODO: Postcondition
         return this.asDataString();
     }
 
@@ -44,6 +57,7 @@ export abstract class AbstractName implements Name {
             const dataComponent = this.nameParser.remask(this.getComponent(i), DEFAULT_DELIMITER);
             dataComponents.push(dataComponent);
         }
+        // TODO: class invariant: valid data string?
         return dataComponents.join(DEFAULT_DELIMITER);
     }
 
@@ -51,7 +65,8 @@ export abstract class AbstractName implements Name {
     // Equality Interface
     // --------------------------------------------------------------------------------------------
 
-    public isEqual(other: Name): boolean {    
+    public isEqual(other: Name): boolean {
+        // TODO: Precondition: valid other Name?   
         return this.asDataString() === other.asDataString() &&
                this.getDelimiterCharacter() === other.getDelimiterCharacter() &&
                this.getNoComponents() == other.getNoComponents();
@@ -60,11 +75,13 @@ export abstract class AbstractName implements Name {
     public getHashCode(): number {
         let hashCode: number = 0;
         const s: string = this.asDataString();
+        // TODO: Postcondition
         for (let i: number = 0; i < s.length; i++) {
             let c: number = s.charCodeAt(i);
             hashCode = (hashCode << 5) - hashCode + c;
             hashCode |= 0;
         }
+        // TODO: Class invarianz
         return hashCode;
     }
 
@@ -73,27 +90,31 @@ export abstract class AbstractName implements Name {
     // --------------------------------------------------------------------------------------------
 
     public isEmpty(): boolean {
+        // TODO: Postcondition: valid no of components
         return this.getNoComponents() === 0;
     }
 
     // ------------------------------------ Getter and Setter -------------------------------------
 
     public getDelimiterCharacter(): string {
+        // TODO: Precondition valid delim
         return this.delimiter;
     }
 
     abstract getNoComponents(): number;
 
     public getComponent(i: number): string {
-        this.assertIndexExclusive(i);
+        IllegalArgumentException.assert(this.isValidIndexExclusive(i));
+        // TODO: Class invarianz: valid component
         return this.doGetComponent(i);
     }
 
     protected abstract doGetComponent(i: number): string;
 
     public setComponent(i: number, c: string): void {
-        this.assertIndexExclusive(i);
+        IllegalArgumentException.assert(this.isValidIndexExclusive(i));
         this.doSetComponent(i, c);
+        // TODO: Class invarianz: valid component
     }
 
     protected abstract doSetComponent(i: number, c: string): void;
@@ -101,8 +122,9 @@ export abstract class AbstractName implements Name {
     // ------------------------------------ Mutation Methods --------------------------------------
 
     public insert(i: number, c: string): void {
-        this.assertIndexInclusive(i);
+        IllegalArgumentException.assert(this.isValidIndexInclusive(i));
         this.doInsert(i, c);
+        // TODO: Class invarianz: valid string
     }
 
     protected abstract doInsert(i: number, c: string): void;
@@ -110,25 +132,34 @@ export abstract class AbstractName implements Name {
     abstract append(c: string): void;
 
     public remove(i: number): void {
-        this.assertIndexExclusive(i);
+        IllegalArgumentException.assert(this.isValidIndexExclusive(i));
         this.doRemove(i);
+        // TODO: Class invarianz: valid string
     }
 
     protected abstract doRemove(i: number): void;
 
     public concat(other: Name): void {
+        // TODO: Precondition: Check Name
         for (let i = 0; i < other.getNoComponents(); i++) {
             const otherComponent = other.getComponent(i);
+            // TODO: Postcondtion: valid component
             const otherDelimiter = other.getDelimiterCharacter();
+            // TODO: Postcondtion: valid delim
             const component = this.nameParser.remask(otherComponent, this.delimiter, otherDelimiter)
-            
+            // TODO: Postcondtion: valid component
             this.append(component);
+            // TODO: Class invarianz: valid string
         }
     }
 
     // --------------------------------------------------------------------------------------------
     // Utility Functions
     // --------------------------------------------------------------------------------------------
+
+    private isValidDelimiter(delimiter: string) {
+        return delimiter.length === 1;
+    }
 
     /**
      * Asserts that the index is within existing component boundaries.
@@ -137,10 +168,8 @@ export abstract class AbstractName implements Name {
      * @param i The index to check.
      * @throws Error if the index is out of bounds.
      */
-    private assertIndexExclusive(i: number): void {
-        if (i < 0 || this.getNoComponents() <= i) {
-            throw new Error("Index out of bounds");
-        }
+    private isValidIndexExclusive(idx: number): boolean {
+        return 0 <= idx && idx < this.getNoComponents();
     }
     
     /**
@@ -150,9 +179,7 @@ export abstract class AbstractName implements Name {
      * @param i The index to check.
      * @throws Error if the index is out of bounds.
      */
-    private assertIndexInclusive(i: number): void {
-        if (i < 0 || this.getNoComponents() < i) {
-            throw new Error("Index out of bounds");
-        }
+    private isValidIndexInclusive(i: number): boolean {
+        return 0 <= i && i <= this.getNoComponents();
     }
 }
