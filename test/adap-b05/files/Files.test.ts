7743a4228f1ae04a1df8756adc86d1026ffc3b68
Philipp Gerlesberger <Gerlesberger.Philipp@gmail.com>
2025-12-07 21:51:30 +0100

    feat/refactor: Add more tests, remove logs.

--- test/adap-b05/files/Files.test.ts
+++ test/adap-b05/files/Files.test.ts
@@ -32,28 +32,6 @@ function createFileSystem(): RootNode {
   return rn;
 }
 
-function createFileSystem2(): RootNode {
-  let rn: RootNode = new RootNode();
-  const names: string[] = ["a", "b", "c"];
-  let dirs: Directory[] = [rn];
-
-
-  for (let i = 0; i < 3; i++) {
-    let currentDirs: Directory[] = [];
-    for (let name of names) {
-      for (let dir of dirs) {
-        let n: Directory = new Directory(name, dir);
-        currentDirs.push(n);
-        let f: File = new File(name, dir);
-      }
-    }
-
-    dirs = currentDirs;
-  }
-
-  return rn;
-}
-
 function createFileSystem3(depth: number = 3): Node[] {
   let rn: RootNode = new RootNode();
   const names: string[] = ["a", "b", "c"];
@@ -77,7 +55,7 @@ function createFileSystem3(depth: number = 3): Node[] {
 }
 
 describe("Parted tree test", () => {
-  it("test 1", () => {
+  it("Test only one leave", () => {
     let n: Node[] = createFileSystem3();
     let as: Node[] = [...n[1].findNodes("a")];
     let asNames: Set<string> = new Set(as.map(a => a.getFullName().asString()))
@@ -91,18 +69,7 @@ describe("Parted tree test", () => {
     }
   });
 
-  it("test 2", () => {
-    let n: Node[] = createFileSystem3();
-    let link: Link = new Link("link", n[1] as Directory, n[0]);
-    let as: Set<Node> = n[1].findNodes("a");
-    let rs: Set<Node> = n[0].findNodes("a");
-    expect(as.size == rs.size).toBe(true);
-    for (let a of as) {
-      expect(rs.has(a)).toBe(true);
-    }
-  });
-
-  it("test 3", () => {
+  it("Test 2 Leaves", () => {
 
     let nodeNames: Set<string> = new Set([
       "/a", "/a/a", "/a/a/a", "/a/b/a", "/a/c/a",
@@ -119,6 +86,17 @@ describe("Parted tree test", () => {
       expect(a.getBaseName()).toBe("a");
       let fullName: string = a.getFullName().asString();
       expect(nodeNames.has(fullName)).toBe(true);
+    }
+  });
+
+  it("Test Link to Root", () => {
+    let n: Node[] = createFileSystem3();
+    let link: Link = new Link("link", n[1] as Directory, n[0]);
+    let as: Set<Node> = n[1].findNodes("a");
+    let rs: Set<Node> = n[0].findNodes("a");
+    expect(as.size == rs.size).toBe(true);
+    for (let a of as) {
+      expect(rs.has(a)).toBe(true);
     }
   });
 
@@ -144,18 +122,49 @@ describe("Basic naming test", () => {
     let fs: RootNode = createFileSystem();
     let ls: Node = [...fs.findNodes("ls")][0];
     let ls2: Node = [...fs.findNodes("ls")][0];
-    console.log(ls2.getFullName().asString())
     let n = new StringName("/usr/bin/ls", '/');
-
     expect(ls.getFullName().asString()).toBe(n.asString());
   });
 
+
   it("test name checking 2", () => {
-    let fs: RootNode = createFileSystem2();
-    let nodes: Node[] = [...fs.findNodes("a")];
-    for (let n of nodes) {
-      console.log(n.getFullName().asString());
-    }
+    let fs: Node[] = createFileSystem3();
+    let nodes: Node[] = [...fs[0].findNodes("a")];
+    expect(nodes.length).toBe(13);
+  });
+
+  it("Test files", () => {
+    let fs: Node[] = createFileSystem3(2);
+    let files: Node[] = [...fs[0].findNodes("file")];
+    expect(files.length).toBe(12);
+  });
+
+  it("Start at file 1", () => {
+    let fs: Node[] = createFileSystem3(1);
+    let file: File = new File("Start", fs[0] as Directory);
+    let nodes: Node[] = [...file.findNodes("a")];
+    expect(nodes.length).toBe(0);
+  });
+
+  it("Start at file 2", () => {
+    let fs: Node[] = createFileSystem3(1);
+    let file: File = new File("Start", fs[0] as Directory);
+    let nodes: Node[] = [...file.findNodes("Start")];
+    expect(nodes.length).toBe(1);
+  });
+
+  it("Start at link 1", () => {
+    let fs: Node[] = createFileSystem3(1);
+    let link: Link = new Link("Start", fs[1] as Directory, fs[0]);
+    let nodes: Node[] = [...link.findNodes("c")];
+    expect(nodes.length).toBe(1);
+  });
+
+  it("Start at link 2", () => {
+    let fs: Node[] = createFileSystem3(1);
+    let link: Link = new Link("Start", fs[1] as Directory, fs[0]);
+    let nodes: Node[] = [...link.findNodes("Start")];
+    expect(nodes.length).toBe(1);
   });
 });
 
@@ -187,18 +196,12 @@ describe("Circular test", () => {
     expect(as.length).toBe(1);
   });
 
-  it("test link name", () => { 
-    let n: Node[] = createFileSystem3(1);
-    let link: Link = new Link("link", n[0] as Directory, n[1]);
-    console.log(link.getBaseName());
-  });
-
   it ("test link size", () => {
     let root: RootNode = new RootNode()
     let a: Node = new Node("a", root);
     let link: Link = new Link("link", root, a);
-    console.log(root.findNodes("link").size); // 0 oder 1?
-    console.log(root.findNodes("a").size); // 1 oder 2?
+    expect(root.findNodes("link").size).toBe(1);
+    expect(root.findNodes("a").size).toBe(1);
   })
 
 });
